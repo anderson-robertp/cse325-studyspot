a3d833c435bd5a1315018aff5c47109946ffdccc
anderson-robertp <anderon-rp@hotmail.com>
2026-07-14 20:04:01 -0500

    Update README with project details and features

---
+++ README.md
@@ -0,0 +1,22 @@
+# CSE-325-Group-Project
+study room reservation app
+
+Website for reserving study rooms in the McKay Library
+
+### Target Audience
+BYU-I students and faculty
+
+### Project Features
+home page with a visual map of the library layout with all available rooms
+* Have a map showing the layout of the rooms with color coding to indicate availability
+* Click on rooms to see a schedule and reserve them
+* Create user sign in authentication
+* Allow users to create, read, update, and delete reservations
+
+
+# Project Members
+* Andrew Jeppesen
+* Spencer Palmer
+* David Martinez Burgos
+* Hyrum Whittier
+* Robert Anderson
--- StudySpot/obj/Debug/net10.0/StudySpot.AssemblyInfo.cs
+++ StudySpot/obj/Debug/net10.0/StudySpot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudySpot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be954d2b31d30b8a905e299dd71806131eb3644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935623d02b0f242a6f27d04888a91585464d3e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudySpot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudySpot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
